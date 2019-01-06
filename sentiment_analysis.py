0293dbc0131a88439c04f99df387fcb42f46e056
Yashbhambhu <43937889+Yashbhambhu@users.noreply.github.com>
2019-01-06 13:17:42 +0530

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,26 +1,27 @@
-import matplotlib.pyplot as plt
-import tweepy
-from textblob import TextBlob
-P=0
-Ne=0
-Nu=0
-consumer_key = '4vYLAmtRXFVc9dp7s2iuuOOID'
+import matplotlib.pyplot as plt #importing the data ploting library
+import tweepy   #importing the tweet fetching library
+from textblob import TextBlob #importing the anlysis performing library 
+P=0  #counter for positive tweets
+Ne=0 #counter for negetive tweets
+Nu=0 #counter for neutral tweets
+#saving variables so that code dose'nt seem to be written in gibberish :) 
+consumer_key = '4vYLAmtRXFVc9dp7s2iuuOOID' 
 consumer_key_secret='770qat6wInRcaV8AytlEeoJCluudAHECtFo7QsfebVW8ZG5zKM'
 access_token = '2476790046-IFCtG5p5uhyIqLn19JjGoXTLmIs5yApgIKSdcMU'
 access_token_secret = 'vPGih9SaGE4twxp9ZtpoWMLYAP7rvYRFHaHhIYkhi7HGk'
-auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret) # authenticating with twitter API
+auth.set_access_token(access_token, access_token_secret) # reaching to outr app that we created
 api = tweepy.API(auth)
 s=raw_input('what do you want to analyze?it can be a #Tag,companay,celebrity etc.\n')
 co=input('how much tweets do you want to analyze?\n')
-#public_tweets = api.search('#MeToo')
 c=0
-for tweet in tweepy.Cursor(api.search,
+for tweet in tweepy.Cursor(api.search,                      #using cursor in tweepy to get tweets it automatically takes care of 'pagination'
                            q = s,count=co,
                            lang = "en").items(co):   
-     print(tweet.text)
-     analysis = TextBlob(tweet.text)
-     print(analysis.sentiment)
+     print(tweet.text) #printing what we've extracted
+     analysis = TextBlob(tweet.text) #analysisng what we've printed
+     print(analysis.sentiment)#printing what we've analysed in mathematical or technical tems.
+    #printing what we've analysed in layman's terms
      if analysis.sentiment[0]>0:
       print( 'Positive')
       P=P+1
@@ -33,7 +34,9 @@ for tweet in tweepy.Cursor(api.search,
       c=c+1
       print ('Neutral')
       Nu=Nu+1 
+      
 print(c)
+#ploting a fancy pie chart of out analysis.
 Z=[P,Ne,Nu]  
 L=['positive','negetive','neutral']     
 plt.pie(Z , labels=L)
